10b391a44c1b34e5e036e70eb3b0ff80e418a2e0
Veinosef <ygrtyusufyasar@gmail.com>
2026-08-22 17:11:57 +0300

    Update CoreBlockWorldHandler.java

--- src/main/java/net/veinosef/coreblock/CoreBlockWorldHandler.java
+++ src/main/java/net/veinosef/coreblock/CoreBlockWorldHandler.java
@@ -11,16 +11,12 @@ public class CoreBlockWorldHandler {
             if (world.getRegistryKey() == ServerWorld.OVERWORLD) {
                 BlockPos corePos = new BlockPos(0, 64, 0);
 
-                // 3x3 Toprak Platform ve Ortada CoreBlock
+                // 3x3 Başlangıç Toprak Platformu ve Ortada İlk Blok
                 if (world.isAir(corePos)) {
                     for (int x = -1; x <= 1; x++) {
                         for (int z = -1; z <= 1; z++) {
                             BlockPos pos = new BlockPos(x, 64, z);
-                            if (x == 0 && z == 0) {
-                                world.setBlockState(pos, CoreBlockMod.CORE_BLOCK.getDefaultState());
-                            } else {
-                                world.setBlockState(pos, Blocks.DIRT.getDefaultState());
-                            }
+                            world.setBlockState(pos, Blocks.DIRT.getDefaultState());
                         }
                     }
                     world.setSpawnPos(new BlockPos(0, 65, 0), 0.0f);
